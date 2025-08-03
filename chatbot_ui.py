754290228cfeeaa5375ae7dd505535ccd7465a73
Pranav Chaudhari <143521321+Pranav6615@users.noreply.github.com>
2025-08-03 11:42:06 +0530

    Update chatbot_ui.py

--- chatbot_ui.py
+++ chatbot_ui.py
@@ -11,11 +11,11 @@ FINE_TUNED_MODEL = "ft:gpt-3.5-turbo-0125:personal::Bzmrn6Yp"
 st.set_page_config(page_title="Hinglish Support Bot", layout="centered")
 st.title("💬 Hinglish Support Bot Demo")
 st.caption(
-    "🧠 This Hinglish Support Bot is a ready-to-integrate customer support assistant, "
-    "fine-tuned for formal Hinglish conversations. It can be customized for any company’s FAQs, "
-    "policies, and support tone.\n\n"
+    "🧠 This Hinglish Support Bot is a ready-to-integrate customer support assistant", 
+    "fine-tuned for formal Hinglish conversations. It can be customized for any company’s FAQs", 
+    "policies, and support tone."\n\n
     "🌐 Why Hinglish? A large segment of Indian users prefer a mix of Hindi and English — "
-    "making support more relatable, accessible, and human.\n\n"
+    "making support more relatable, accessible, and human."\n\n
     "✅ Try asking about orders, payments, returns, or delivery in Hinglish. "
     "You’ll see how smoothly it handles real-world support scenarios."
     "You can ask upto 5 queries in this demo"
@@ -62,4 +62,5 @@ if prompt := st.chat_input("Type your question here..."):
     # Display and store assistant response
     st.session_state.messages.append({"role": "assistant", "content": reply})
     st.chat_message("assistant").markdown(reply)
+
 
